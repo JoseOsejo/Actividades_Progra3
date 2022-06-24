7ef044278e8acdee1deae4f6b9e5ffe137250258
Jose Carlos Osejo <jcoq2003@gmail.com>
2022-06-23 22:11:31 -0600

    Actividades Programacion 3

---
+++ Actividad6/ListaCircular.cpp
@@ -0,0 +1,116 @@
+#include "ListaCircular.h"
+
+#include <iostream>
+
+using std::cout;
+
+ListaCircular::ListaCircular() : primero(nullptr), ultimo(nullptr)
+{}
+
+bool ListaCircular::estaVacia()
+{
+	return primero == nullptr;
+}
+
+void ListaCircular::agregarElemento(int _valor)
+{
+	Nodo* nuevo = new Nodo(_valor, nullptr, nullptr);
+
+	if (estaVacia())
+	{
+		primero = nuevo;
+		ultimo = nuevo;
+
+		primero->setAnterior(ultimo);
+		ultimo->setSiguiente(primero);
+	}
+	else
+	{
+		nuevo->setAnterior(ultimo);
+		ultimo->setSiguiente(nuevo);
+
+		ultimo = nuevo;
+
+		ultimo->setSiguiente(primero);
+		primero->setAnterior(ultimo);
+	}
+}
+
+void ListaCircular::imprimirLista()
+{
+	cout << std::endl;
+	if (estaVacia())
+	{
+		cout << "{ lista vacia }\n";
+		return;
+	}
+
+
+	Nodo* actual = primero;
+
+	do {
+		cout << "[ " << actual->getValor() << " ] ";
+		actual = actual->getSiguiente();
+	} while (actual != primero);
+}
+
+void ListaCircular::imprimirListaRev()
+{
+	if (estaVacia())
+		return;
+
+
+	Nodo* actual = ultimo;
+
+	cout << std::endl;
+
+
+	do {
+		cout << "[ " << actual->getValor() << " ] ";
+		actual = actual->getAnterior();
+	} while (actual != ultimo);
+}
+
+bool ListaCircular::eliminarElemento(int _valor)
+{
+	if (estaVacia())
+		return false;
+
+	Nodo* actual = primero;
+
+	do
+	{
+		if (actual->getValor() == _valor)
+		{
+			if (actual == primero && actual == ultimo)
+			{
+				primero = ultimo = nullptr;
+			}
+			else if (actual == primero)
+			{
+				primero = actual->getSiguiente();
+				primero->setAnterior(ultimo);
+				ultimo->setSiguiente(primero);
+			}
+			else if (actual == ultimo)
+			{
+				ultimo = actual->getAnterior();
+				ultimo->setSiguiente(primero);
+				primero->setAnterior(ultimo);
+			}
+			else
+			{
+				actual->getAnterior()->setSiguiente(actual->getSiguiente());
+				actual->getSiguiente()->setAnterior(actual->getAnterior());
+			}
+
+			delete actual;
+			return true;
+		}
+
+		actual = actual->getSiguiente();
+
+	} while (actual != primero);
+
+	return false;
+}
---
+++ Actividad6/main.cpp
@@ -0,0 +1,37 @@
+#include <conio.h>
+#include "ListaCircular.h"
+
+int main()
+{
+	ListaCircular lc(10);
+
+	lc.agregarElemento(7);
+	lc.agregarElemento(3);
+	lc.agregarElemento(2);
+
+	lc.imprimirLista();
+	lc.imprimirListaRev();
+
+	lc.eliminarElemento(3);
+	lc.imprimirLista();
+
+	lc.eliminarElemento(2);
+	lc.imprimirLista();
+
+	lc.eliminarElemento(7);
+	lc.imprimirLista();
+
+	lc.agregarElemento(11);
+	lc.agregarElemento(9);
+	lc.agregarElemento(3);
+	lc.imprimirLista();
+
+	//(posicion, valor de nodo)
+	lc.insertarElemento(0, 5);
+	lc.insertarElemento(3, 2);
+
+	//[5] [11] [9] [3]
+	//[5] [11] [9] [2] [3]
+
+	_getch();
+}
